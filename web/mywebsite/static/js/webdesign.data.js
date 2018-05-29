149a86c352e1659d35287223c1433cc21aea23a9
psycho909 <psychosocial909@gmail.com>
2018-05-30 00:22:14 +0800

    123

--- web/mywebsite/static/js/webdesign.data.js
+++ web/mywebsite/static/js/webdesign.data.js
@@ -1,4 +1,5 @@
 [
+	{"href":"https://psycho909.github.io/resumeweb/index.html","img":"./static/images/webdesign/resumeweb.jpg","title":"履歷表","type":"HTML RWD"},
 	{"href":"https://psycho909.github.io/sengoku/index.html","img":"./static/images/webdesign/sengoku.jpg","title":"戰國時代","type":"HTML"},
 	{"href":"https://travel-website.herokuapp.com/","img":"./static/images/webdesign/nodeweb.jpg","title":"旅遊網","type":"Node Express RWD MongoDB"},
 	{"href":"https://psycho909.github.io/cloth2_demo/index.html","img":"./static/images/webdesign/20180427.jpg","title":"布平方手作教室","type":"RWD"},
